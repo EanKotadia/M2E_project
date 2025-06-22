454c669254354525471f9c493dca739ba912ce07
Ean Kotadia <kotadia.ean@gmail.com>
2025-06-22 17:17:26 +0530

    Added new project page and images

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template;
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# Database config
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+
+# ROUTES
 
 @app.route('/')
 def index():
@@ -18,6 +21,10 @@ def logs():
 def about():
     return render_template('about.html')
 
-# Run the Flask app
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+# MAIN
 if __name__ == '__main__':
     app.run(debug=True)
